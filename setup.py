f60ba7f5658e9d7f9d739aba59cc4c0bc89ac86a
Géry Casiez <3748153+casiez@users.noreply.github.com>
2022-04-04 21:19:58 +0200

    minor

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='latexSAK',  
-     version='0.1.0',
+     version='0.1.2',
      scripts=['latexSAK'] ,
      author="Géry Casiez",
      author_email="gery.casiez@gmail.com",
